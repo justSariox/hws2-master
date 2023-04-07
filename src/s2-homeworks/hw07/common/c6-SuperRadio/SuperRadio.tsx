9c1e03d49c9582f5b896622bf8abff81e3b2b501
justSariox <87971082+justSariox@users.noreply.github.com>
2023-04-07 17:49:57 +0300

    Update SuperRadio.tsx

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -63,7 +63,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       {...spanProps}
                       className={spanClassName}
                   >
-                      {o.value }
+                      {o.value}
                   </span>
               </label>
           ))
