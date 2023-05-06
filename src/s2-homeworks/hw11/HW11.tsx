5f011d22f6dc0045b855f990f5ba21f1c019b84d
justSariox <87971082+justSariox@users.noreply.github.com>
2023-05-06 17:14:56 +0300

    hw-11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -13,13 +13,16 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
 
     const change = (event: Event, value: number | Array<number>) => {
-        if (Array.isArray(value)) {
+        if (!Array.isArray(value)) {
+            setValue1(value)
+            setValue2([value, value2[1]])
+        } else {
             setValue1(value[0])
-            setValue2(value[1])
-        } else setValue1(value)
+            setValue2(value)
+        }
 
 
     }
@@ -44,11 +47,11 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            value={[value1, value2]}
+                            value={value2}
                             onChange={change}
                             disableSwap
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.number}>{value2[value2.length - 1]}</span>
                     </div>
                 </div>
             </div>
