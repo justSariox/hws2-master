d23129829a44ab66fc6326e0ad762d56ece13ec0
justSariox <87971082+justSariox@users.noreply.github.com>
2023-05-06 17:06:42 +0300

    hw-11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
 import React, {ChangeEvent, useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState, saveState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -16,10 +16,10 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: Event, value: number | Array<number>) => {
-       if (Array.isArray(value)) {
-           setValue1(value[0])
-           setValue2(value[1])
-       } else setValue1(value)
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else setValue1(value)
 
 
     }
@@ -46,6 +46,7 @@ function HW11() {
                             id={'hw11-double-slider'}
                             value={[value1, value2]}
                             onChange={change}
+                            disableSwap
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
